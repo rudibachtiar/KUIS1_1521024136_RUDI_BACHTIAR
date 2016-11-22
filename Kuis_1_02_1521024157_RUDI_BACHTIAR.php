ce41adca299fc6eab5186911474e169963c0fd4d
rudibachtiar88@gmail.com <bojonegoro123>
2016-11-22 20:07:31 +0700

    rudi

---
+++ Kuis_1_02_1521024157_RUDI_BACHTIAR.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>AKN Bojonegoro</title>
+</head>
+<body>
+<?php
+$kalimat = array
+$kalimat = (array("RUDI 1521024157 BACHTIAR");
+echo $kalimat[0][0]. .$kalimat[0][1]. .$kalimat[0][2].;
+?>
+</body>
+</html>
